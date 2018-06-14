815487045fac81f369ab732de0e628d84cd28c68
mac <mac@macs-MacBook-Pro.local>
2018-06-14 16:16:52 -0600

    Commit 1 - Verbal questions

---
+++ solutions.js
@@ -0,0 +1,19 @@
+//Verbal Questions:
+
+
+//1. Parameters are the aliases for the values that are passed to a function, while arguments are the 
+//actual values.  
+
+//2. Within a function, return ends a function and returns a value according to the code within the 
+//function, while console.log only prints the argument assigned to it.
+
+
+
+
+
+
+
+
+
+
+
