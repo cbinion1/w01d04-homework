2b4e6a0278a9d5a8ae0d40e7ad27566bacaa1af0
mac <mac@macs-MacBook-Pro.local>
2018-06-14 20:22:59 -0600

    Commit 2.1: Palindrome

--- solutions.js
+++ solutions.js
@@ -9,6 +9,27 @@
 
 
 
+//Palindrome
+
+
+const Palindrome = (str) => {
+    
+  let strLen = str.length;
+  
+    if (strLen === 0 || strLen === 1) {
+        return true;
+    }
+    
+    if (str[0] === str[strLen - 1]) {
+        return isPalindrome( str.slice(1, strLen - 1) );
+    }
+    
+    return false;
+};
+
+console.log(Palindrome("blorg"));
+
+
 
 
 
